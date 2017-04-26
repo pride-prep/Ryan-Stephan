b9e529a33ad8248464a599aa91b00fe182164cd1
DatHawksFan24 <ryans@my.prideprepschool.org>
2017-04-26 13:04:05 -0700

    Add files via upload
    HW1 submitted.

---
+++ HW1/HW1/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  HW1
+//
+//  Created by Student on 4/26/17.
+//  Copyright © 2017 Student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var homework1: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func button1(_ sender: UIButton) {
+    }
+    @IBAction func button2(_ sender: UIButton) {
+    }
+    @IBAction func button3(_ sender: UIButton) {
+    }
+
+}
+
