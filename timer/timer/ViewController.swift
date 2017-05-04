ae5974f2909a2c00802f800f89d9c3964a6d5812
DatHawksFan24 <ryans@my.prideprepschool.org>
2017-05-04 13:04:32 -0700

    Add files via upload

---
+++ timer/timer/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  timer
+//
+//  Created by Student on 5/4/17.
+//  Copyright © 2017 Student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    @IBOutlet weak var label1: UILabel!
+
+    @IBAction func startButton(_ sender: Any) {
+    }
+    @IBAction func button3(_ sender: UIButton) {
+    }
+    @IBAction func button2(_ sender: UIButton) {
+    }
+    @IBAction func button1(_ sender: UIButton) {
+    
+    }
+    
+}
